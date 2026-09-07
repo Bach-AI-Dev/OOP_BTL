c936835b73d23c70e5bc77e44146ce6ce34b23e9
Bach-AI-Dev <nguyenduong115870@gmail.com>
2026-09-07 10:25:05 +0700

    nhanh moi nguoi tai ve

---
+++ src/test/java/com/baitapnhom/courseweb/CoursewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baitapnhom.courseweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
