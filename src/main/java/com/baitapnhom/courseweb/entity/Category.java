e263df3a24966a19e5ec43722e3f2e6314b4015c
kithanhdat-cmyk <kieuthanhdat160620061@gmail.com>
2026-09-20 21:22:31 +0700

    Update Category.java

---
+++ src/main/java/com/baitapnhom/courseweb/entity/Category.java
@@ -0,0 +1,41 @@
+package OOP_BTL_develop.demo.entity;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(nullable = false, unique = true )
+    private String name;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Course> coursers;
+
+    public Category() {
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCoursers() {
+        return coursers;
+    }
+
+    public void setCoursers(List<Course> coursers) {
+        this.coursers = coursers;
+    }
+}
