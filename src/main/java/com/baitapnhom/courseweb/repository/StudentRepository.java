2a90b91689b0bdd032474c1437c34fb34212ce7e
Nguyễn Tùng Duơng <nguyenduong115870@gmail.com>
2026-09-20 22:34:35 +0700

    upload StudentRepo

---
+++ src/main/java/com/baitapnhom/courseweb/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.baitapnhom.courseweb.repository;
+
+import com.baitapnhom.courseweb.entity.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
