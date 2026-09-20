88702af22ac394fae23de23c70281e0b06a43009
kithanhdat-cmyk <kieuthanhdat160620061@gmail.com>
2026-09-20 21:22:59 +0700

    Update Course.java

--- src/main/java/com/baitapnhom/courseweb/entity/Course.java
+++ src/main/java/com/baitapnhom/courseweb/entity/Course.java
@@ -1,1 +1,73 @@
+package OOP_BTL_develop.demo.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue( strategy = GenerationType.UUID)
+        private String id;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String price;
+    private String status;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    public Course() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
+
 
