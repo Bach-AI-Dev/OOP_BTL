29e9f4786553b84a127d89cd58471688debcc2ec
Nguyễn Tùng Duơng <nguyenduong115870@gmail.com>
2026-09-20 22:25:05 +0700

    Refactor Student entity with updated fields and methods

--- src/main/java/com/baitapnhom/courseweb/entity/Student.java
+++ src/main/java/com/baitapnhom/courseweb/entity/Student.java
@@ -1,43 +1,32 @@
 package com.baitapnhom.courseweb.entity;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "students")
 public class Student {
-
+    
     @Id
     @Column(name = "student_id")
-    private Integer id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @MapsId // Báo cho Spring Boot biết: Hãy lấy ID của User làm ID của Student
-    @JoinColumn(name = "student_id")
+    private String studentId;
+    
+    @OneToOne
+    @JoinColumn(name = "student_id", nullable = false, insertable = false, updatable = false)
     private User user;
-
-    @Column(name = "created_at")
+    
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    // Constructors
-    public Student() {
-    }
-
-    // Getters and Setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+    
+    // Constructor, Getter, Setter
+    public Student() {}
+    
+    public String getStudentId() {return studentId;}
+    public void setStudentId(String studentId) {this.studentId = studentId;}
+    
+    public User getUser() {return user;}
+    public void setUser(User user) {this.user = user;}
+    
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
 }
