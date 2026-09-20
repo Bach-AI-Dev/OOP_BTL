9a860f41dcccf9ade657fd614f384c45776b602b
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-20 21:14:38 +0700

    Manh

---
+++ src/main/java/com/baitapnhom/courseweb/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.baitapnhom.courseweb.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.baitapnhom.courseweb.entity.User;
+
+// Kế thừa siêu nạp extends JpaRepository<User, Long>
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // existsByUsername và existsByEmail trả về true nếu tìm thấy ngược lại trả về
+    // false
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    // Lấy toàn bộ thông tin tài khoản dựa vào tên đăng nhập hoặc email.
+    // bọc trong kiểu Optional<User> tránh lỗi NullPointerException
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
