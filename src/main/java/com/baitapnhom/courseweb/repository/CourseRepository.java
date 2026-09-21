6d3ca5775cf9a1c177bed54cd35a415b3d1a695f
kithanhdat-cmyk <kieuthanhdat160620061@gmail.com>
2026-09-21 23:45:49 +0700

    Update CourseRepository.java

--- src/main/java/com/baitapnhom/courseweb/repository/CourseRepository.java
+++ src/main/java/com/baitapnhom/courseweb/repository/CourseRepository.java
@@ -1,7 +1,8 @@
-package OOP_BTL_develop.demo.repository;
+package com.baitapnhom.courseweb.repository;
 
-import OOP_BTL_develop.demo.entity.Course;
+import com.baitapnhom.courseweb.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CourseRepository extends JpaRepository<Course, String> {
 }
